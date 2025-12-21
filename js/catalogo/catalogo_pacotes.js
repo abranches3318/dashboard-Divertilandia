13f24f803a22b85671b9393c2f7cbc4c87f71c1c
abranches3318 <gabrielnpc2010@hotmail.com>
2025-12-21 02:15:16 -0300

    Update catalogo_pacotes.js

--- js/catalogo/catalogo_pacotes.js
+++ js/catalogo/catalogo_pacotes.js
@@ -1,1 +1,13 @@
+function carregarPacotes() {
+  const el = document.getElementById("catalogo-content");
+  el.innerHTML = `
+    <div class="catalogo-header">
+      <h2>Pacotes</h2>
+      <button class="btn btn-primary">Novo pacote</button>
+    </div>
 
+    <div class="catalogo-lista">
+      <p>Nenhum pacote carregado.</p>
+    </div>
+  `;
+}
